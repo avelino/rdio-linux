ddfa34afa811681823b8d69c8ed0658191a95b22
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-09-06 16:05:00 -0300

    Add new descriptions in setup

--- rdio/player/__init__.py
+++ rdio/player/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 __author__ = 'Thiago Avelino'
+__author_email__ = 'thiagoavelinoster@gmail.com'
+__description__ = 'Rdio for linux'
 __license__ = 'BSD License'
 __version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -14,8 +14,16 @@ from setuptools import setup
 setup(
     name='rdio.player',
     version=rdio.player.__version__,
+    description=rdio.player.__description__,
     url='http://rdio.com/',
     author=rdio.player.__author__,
+    author_email=rdio.player.__author_email__,
     license=rdio.player.__license__,
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: Utilities'],
     scripts=['rdio/player/rdio-player'],)
 #    install_requires=['pygtk', 'pywebkitgtk'],)
